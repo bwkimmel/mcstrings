df8e942e04db9724742603de2f9bc6a0c1de4306
Brad Kimmel <intrepidca@gmail.com>
2021-03-19 19:59:06 -0400

    Add comments.

--- mcstrings.go
+++ mcstrings.go
@@ -1,3 +1,4 @@
+// mcstrings is a tool for extracting strings from a Minecraft world.
 package main
 
 import (
@@ -27,13 +28,18 @@ var (
 	invert = flag.Bool("invert", false, "Output entries *not* matching the filter")
 	header = flag.Bool("header", true, "Include header row in the output")
 
+	// compressionFilters maps the compression type tag to the corresponding
+	// function for decorating a Reader for decompressing the chunk data. See
+	// https://minecraft.gamepedia.com/Region_file_format#Chunk_data.
 	compressionFilters = map[int8]func(io.Reader) (io.ReadCloser, error){
 		1: newGZipFilter,
 		2: zlib.NewReader,
 		3: newIdentFilter,
 	}
 
-	outputFilters = map[string]func(_, _ string) bool{
+	// outputFilters defines the predicates used for filtering NBT data from the
+	// emitted results.
+	outputFilters = map[string]func(k, v string) bool{
 		"all":       func(_, _ string) bool { return true },
 		"user_text": containsUserText,
 	}
@@ -44,9 +50,11 @@ var (
 
 type output struct {
 	csv    *csv.Writer
-	filter func(_, _ string) bool
+	filter func(k, v string) bool
 }
 
+// validOutputFilters returns a comma-separated list of valid output filter
+// names for usage documentation.
 func validOutputFilters() string {
 	var names []string
 	for k, _ := range outputFilters {
@@ -55,10 +63,16 @@ func validOutputFilters() string {
 	return strings.Join(names, ", ")
 }
 
+// clean canonicalizes a string for comparisons by trimming whitespace and
+// converting it to lowercase.
 func clean(s string) string {
 	return strings.ToLower(strings.TrimSpace(s))
 }
 
+// containsUserText determines if a NBT entry likely contains user-generated
+// text. This includes sign text, book contents & titles, renamed items, etc.,
+// but excludes entries with empty values (empty strings, null JSON objects,
+// signs with empty text).
 func containsUserText(k, v string) bool {
 	v = clean(v)
 	if v == "" {
@@ -90,41 +104,17 @@ func containsUserText(k, v string) bool {
 	return false
 }
 
+// newGZipFilter creates a new filter for GZip-encoded chunks.
 func newGZipFilter(r io.Reader) (io.ReadCloser, error) {
 	return gzip.NewReader(r)
 }
 
+// newIdentFilter creates a new filter for uncompressed chunks.
 func newIdentFilter(r io.Reader) (io.ReadCloser, error) {
 	return ioutil.NopCloser(r), nil
 }
 
-func main() {
-	flag.Parse()
-	if *world == "" {
-		log.Fatal("--world is required")
-	}
-	of, ok := outputFilters[*filter]
-	if !ok {
-		log.Fatalf("Invalid value for --filter (%s), must be one of %s.", *filter, validOutputFilters())
-	}
-	if *invert {
-		orig := of
-		of = func(k, v string) bool {
-			return !orig(k, v)
-		}
-	}
-	out := &output{
-		csv:    csv.NewWriter(os.Stdout),
-		filter: of,
-	}
-	if *header {
-		out.csv.Write([]string{"dimension", "chunk_x", "chunk_z", "nbt_path", "value"})
-	}
-	if err := readWorld(*world, out); err != nil {
-		log.Fatalf("ERROR: %v", err)
-	}
-}
-
+// join combines two segments of an NBT path.
 func join(a, b string) string {
 	if len(b) == 0 {
 		return a
@@ -135,6 +125,19 @@ func join(a, b string) string {
 	return a + "/" + b
 }
 
+// findStrings enumerates the strings within an NBT object, calling the
+// provided callback function with the path and value of each string.
+//
+// If x is a string (TAG_String), cb is invoked with the value of the string
+// itself.
+// If x is a []interface{} (TAG_List), findStrings searches for strings within
+// each element.
+// If x is a map[string]interface{} (TAG_Compound), findStrings searches for
+// strings within each value in the map.
+// If x is a numeric type or an array of numeric types, then there are no
+// strings.
+//
+// See https://minecraft.gamepedia.com/NBT_format
 func findStrings(x interface{}, cb func(path, value string)) {
 	switch value := x.(type) {
 	case string:
@@ -160,6 +163,9 @@ func findStrings(x interface{}, cb func(path, value string)) {
 	}
 }
 
+// readWorld processes the Minecraft world contained in the specified path. The
+// path should point to the directory containing the world's level.dat file.
+// See https://minecraft.gamepedia.com/Java_Edition_level_format.
 func readWorld(path string, out *output) error {
 	if err := readDimension(0, filepath.Join(path, "region"), out); err != nil {
 		return err
@@ -173,6 +179,10 @@ func readWorld(path string, out *output) error {
 	return nil
 }
 
+// readDimension processes the Minecraft dimension contained in the specified
+// path. The path should point to the directory containing the .mca files for
+// the dimension. Dim indicates which dimension is being processed, and should
+// be 0 for overworld, -1 for nether, and 1 for the end.
 func readDimension(dim int, path string, out *output) error {
 	dir, err := os.ReadDir(path)
 	if err != nil {
@@ -196,6 +206,11 @@ func readDimension(dim int, path string, out *output) error {
 	return nil
 }
 
+// readRegion processes a single region contained in the specified file. The
+// path should point to an .mca file. Dim indicates the dimension containing
+// this region (see readDimension). X and Z are the coordinates of the region
+// (which are part of the file name).
+// See https://minecraft.gamepedia.com/Region_file_format.
 func readRegion(dim, x, z int, path string, out *output) error {
 	f, err := os.Open(path)
 	if err != nil {
@@ -203,6 +218,11 @@ func readRegion(dim, x, z int, path string, out *output) error {
 	}
 	defer f.Close()
 
+	// The first 4kB contains 1024 location entries, which indicate where in this
+	// file to find the data for each of the 1024 chunks (32 x 32) in this region.
+	// Each location entry contains a 3-byte file offset (in units of 4k sectors)
+	// and a one byte sector count.
+	// See https://minecraft.gamepedia.com/Region_file_format#Chunk_location.
 	locs := make([]uint32, 1024)
 	if err := binary.Read(f, binary.BigEndian, &locs); err != nil {
 		return fmt.Errorf("cannot read location data from region file %q: %v", path, err)
@@ -242,20 +262,28 @@ func readRegion(dim, x, z int, path string, out *output) error {
 	return nil
 }
 
+// readChunk reads chunk data and returns a map containing the chunk's NBT tree.
+// See https://minecraft.gamepedia.com/Region_file_format#Chunk_data,
+// https://minecraft.gamepedia.com/Chunk_format.
 func readChunk(r io.Reader) (map[string]interface{}, error) {
 	var (
 		length      int32
 		compression int8
 	)
+	// The first four bytes of the chunk contain the (compressed) length,
+	// excluding these four bytes, but including the compression type below.
 	if err := binary.Read(r, binary.BigEndian, &length); err != nil {
 		if err == io.EOF {
 			return nil, err
 		}
 		return nil, fmt.Errorf("cannot read chunk length: %v", err)
 	}
+	// The next byte contains the compression type.
 	if err := binary.Read(r, binary.BigEndian, &compression); err != nil {
-		return nil, fmt.Errorf("cannot read compression tag: %v", err)
+		return nil, fmt.Errorf("cannot read compression type: %v", err)
 	}
+	// The remaining length-1 bytes contains the (possibly-compressed) chunk data
+	// in NBT format.
 	cf, ok := compressionFilters[compression]
 	if !ok {
 		return nil, fmt.Errorf("invalid compression tag: %d", compression)
@@ -278,4 +306,31 @@ func readChunk(r io.Reader) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("cannot decode NBT data: %v", err)
 	}
 	return m, nil
+}
+
+func main() {
+	flag.Parse()
+	if *world == "" {
+		log.Fatal("--world is required")
+	}
+	of, ok := outputFilters[*filter]
+	if !ok {
+		log.Fatalf("Invalid value for --filter (%s), must be one of %s.", *filter, validOutputFilters())
+	}
+	if *invert {
+		orig := of
+		of = func(k, v string) bool {
+			return !orig(k, v)
+		}
+	}
+	out := &output{
+		csv:    csv.NewWriter(os.Stdout),
+		filter: of,
+	}
+	if *header {
+		out.csv.Write([]string{"dimension", "chunk_x", "chunk_z", "nbt_path", "value"})
+	}
+	if err := readWorld(*world, out); err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
 }
